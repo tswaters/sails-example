58694ee9f26e8551915ad410c0846294b3b0a817
Tyler Waters <tyler.waters@gmail.com>
2015-05-30 22:37:30 -0700

    Removes mocks (unused)

--- test/mocks/index.js
+++
@@ -1,9 +0,0 @@
-
-'use strict';
-
-
-module.exports.models = require('./mocks/models');
-module.exports.controllers = require('./mocks/controllers');
-module.exports.services = require('./mocks/services');
-module.exports.reqres = require('./mocks/reqres');
-module.exports.sails = require('./mocks/sails');
