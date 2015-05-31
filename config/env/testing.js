09e1c96579db30c9def76ba85837b0d1990d05c3
Tyler Waters <tyler.waters@gmail.com>
2015-05-30 22:21:33 -0700

    Updates connections to use mongodb for dev, disk for test

---
+++ config/env/testing.js
@@ -0,0 +1,16 @@
+/**
+ * Test environment settings
+ */
+
+module.exports = {
+
+  /***************************************************************************
+   * Set the default database connection for models in the development       *
+   * environment (see config/connections.js and config/models.js )           *
+   ***************************************************************************/
+  models: {
+    connection: 'disk',
+    migrate: 'drop'
+  }
+
+};
