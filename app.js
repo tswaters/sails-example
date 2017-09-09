676bdf6b2e266a24040d7b880985a805b7802c8c
Tyler Waters <tyler.waters@gmail.com>
2017-09-09 16:32:42 -0700

    Drop ExceptionService, errors are now global
    ALso package-lock is dumb

--- app.js
+++ app.js
@@ -3,5 +3,7 @@
 const sails = require('sails')
 const sailsConfig = require('./sails.config')
 
+require('./lib/exceptions')
+
 process.chdir(__dirname)
 sails.lift(sailsConfig())
--- sails.config.js
+++ sails.config.js
@@ -2,33 +2,9 @@
 
 const _ = require('lodash')
 const rc = require('rc')
-const buildDictionary = require('sails-build-dictionary')
 
 module.exports = overrides => {
   const config = {}
-
-  _.merge(config, {
-
-    /**
-     * Whoa who knew you could overload how things are loaded.
-     * This is identical to sails/lib/hooks/moduleloader/index.js#L370-L377
-     * But with once difference, "dont call bindToSails on the services!"
-     * This allows for checking instanceof on returned objects.
-     */
-    moduleLoaderOverride (sails) {
-      return {
-        loadServices (cb) {
-          buildDictionary.optional({
-            dirname: sails.config.paths.services,
-            filter: /(.+)\.(js|coffee|litcoffee)$/,
-            depth: 1,
-            caseSensitive: true
-          }, cb)
-        }
-      }
-    }
-  })
-
   _.merge(config, overrides)
   _.merge(config, rc('sails'))
   return config
