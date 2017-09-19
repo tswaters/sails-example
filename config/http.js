9051df8ad28d706a7e0d26c6f8333ad53015771f
Tyler Waters <tyler.waters@gmail.com>
2017-09-18 20:32:29 -0700

    Update to use pino for logging

--- config/http.js
+++ config/http.js
@@ -12,86 +12,39 @@
 'use strict'
 
 exports.http = {
-
   middleware: {
+
+    404: (req, res, next) => {
+      next(new NotFound('GENERIC', [req.path]))
+    },
+
     500: (err, req, res, next) => {
       if (res.headersSent) { return next(err) }
-      sails.log.error(err)
+      if (err.status === 500) {
+        sails.log.error(err)
+      }
       res.notOk(err)
-    }
+    },
+
+    initializeLogger: (req, res, next) => {
+      const getTransactionKey = req => req.session.id
+      sails.hooks.pino.requestLogger(getTransactionKey)(
+        req,
+        res,
+        () => {
+          req.log.verbose(req)
+          next()
+        }
+      )
+    },
+
+    order: [
+      'www', 'favicon', // static files match first/quick
+      'cookieParser', 'session', // parse out user info
+      'bodyParser', 'handleBodyParserError', // handle body
+      'initializeLogger',
+      'router', // sails router
+      '404', '500' // error handling
+    ]
   }
-
-  /****************************************************************************
-  *                                                                           *
-  * Express middleware to use for every Sails request. To add custom          *
-  * middleware to the mix, add a function to the middleware config object and *
-  * add its key to the "order" array. The $custom key is reserved for         *
-  * backwards-compatibility with Sails v0.9.x apps that use the               *
-  * `customMiddleware` config option.                                         *
-  *                                                                           *
-  ****************************************************************************/
-
-  // middleware: {
-
-  /***************************************************************************
-  *                                                                          *
-  * The order in which middleware should be run for HTTP request. (the Sails *
-  * router is invoked by the "router" middleware below.)                     *
-  *                                                                          *
-  ***************************************************************************/
-
-  // order: [
-  //   'startRequestTimer',
-  //   'cookieParser',
-  //   'session',
-  //   'myRequestLogger',
-  //   'bodyParser',
-  //   'handleBodyParserError',
-  //   'compress',
-  //   'methodOverride',
-  //   'poweredBy',
-  //   '$custom',
-  //   'router',
-  //   'www',
-  //   'favicon',
-  //   '404',
-  //   '500'
-  // ],
-
-  /****************************************************************************
-  *                                                                           *
-  * Example custom middleware; logs each request to the console.              *
-  *                                                                           *
-  ****************************************************************************/
-
-  // myRequestLogger: function (req, res, next) {
-  //     console.log("Requested :: ", req.method, req.url);
-  //     return next();
-  // }
-
-
-  /***************************************************************************
-  *                                                                          *
-  * The body parser that will handle incoming multipart HTTP requests. By    *
-  * default as of v0.10, Sails uses                                          *
-  * [skipper](http://github.com/balderdashy/skipper). See                    *
-  * http://www.senchalabs.org/connect/multipart.html for other options.      *
-  *                                                                          *
-  ***************************************************************************/
-
-  // bodyParser: require('skipper')
-
-  // },
-
-  /***************************************************************************
-  *                                                                          *
-  * The number of seconds to cache flat files on disk being served by        *
-  * Express static middleware (by default, these files are in `.tmp/public`) *
-  *                                                                          *
-  * The HTTP static cache is only active in a 'production' environment,      *
-  * since that's the only time Express will cache flat-files.                *
-  *                                                                          *
-  ***************************************************************************/
-
-  // cache: 31557600000
 }
---
+++ config/pino.js
@@ -0,0 +1,6 @@
+
+'use strict'
+
+exports.pino = {
+  sails: true
+}
